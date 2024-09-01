d37d9f18a0f8414170c26e09815c44757f7e685f
Anuraj-IND <anuraj.m.11.04@mail.com>
2024-09-02 00:51:15 +0530

    my second commit

--- portfolio/home/urls.py
+++ portfolio/home/urls.py
@@ -3,5 +3,8 @@ from django.urls import path,include
 from home import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('contact', views.contact, name='contact'),
+    path('projects', views.projects, name='projects'),
+    path('about', views.aboutus, name='aboutus')
 ]
--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("This is my home page")
+def contact(request):
+    return HttpResponse("This is my contact page (/contact)")    
+def aboutus(request):
+    return HttpResponse("This is my about us page (/about)")
+def projects(request):
+    return HttpResponse("This is my projects page (/projects)")        
