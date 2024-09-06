da0a1123200f5e4b62d3985b8cd6abc8e1a2f45e
Anuraj-IND <anuraj.m.11.04@mail.com>
2024-09-07 00:33:48 +0530

    improvements

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from home.models import Contact
 # Create your views here.
 def home(request):
     d={'name':"Anuraj",'course':"Django",'language':"python"}
@@ -9,3 +10,16 @@ def about(request):
     return render(request,'about.html')
 def project(request):
     return render(request,'project.html')        
+def contactadd(request):
+    try:
+        if request.method == 'POST':
+            name=request.POST['name']
+            mail=request.POST['mail']
+            number=request.POST['number']
+            message=request.POST['message']
+            print(name,mail,contact,message)
+            data=Contact.objects.all()
+            print("added")
+            return render(request,'contact.html')    
+    except Exception as e:
+        return HttpResponse(e)
